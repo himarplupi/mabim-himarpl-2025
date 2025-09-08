bdcc398f80febc6aa91bc1efc8f237cf4a5f61dd
Bintang Kurniawan <bintang.kurniawannn.98@gmail.com>
2025-09-08 14:39:35 +0700

    faq

---
+++ src/components/common/faq-accordions.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { useState } from "react";
+
+import { motion as Motion } from "motion/react";
+
+import { cn } from "@/lib/utils";
+
+import * as AccordionPrimitive from "@radix-ui/react-accordion";
+import { ChevronDownIcon } from "@radix-ui/react-icons";
+
+const faqs = [
+  {
+    id: "item-1",
+    question: "Berapa Lama Mabim Dilaksanakan?",
+    answer: "Mabim dilaksanakan selama 4 hari dalam kurun waktu 1 bulan, dimana setiap pelaksanaannya diadakan pada hari Sabtu, kecuali day 3 yang dilaksanakan pada hari Minggu.",
+  },
+  {
+    id: "item-2",
+    question: "Apa Kepanjangan dari MABIMRPL?",
+    answer: "Kepanjangan dari Mabim adalah Masa Bimbingan. Jadi, Mabim RPL artinya Masa Bimbingan yang dilaksanakan untuk mahasiswa baru dengan Prodi RPL atau Rekayasa Perangkat Lunak",
+  },
+  {
+    id: "item-3",
+    question: "Apa manfaat dari MABIMRPL?",
+    answer: "Mabim sangat berguna untuk memperdalam pemahaman mahasiswa baru terkait prodi yang tengah mereka tempuh, jadi jangan sampai ga ikut ya!",
+  },
+  {
+    id: "item-4",
+    question: "Apakah MABIMRPL Wajib?",
+    answer: "Mabim ini diwajibkan untuk seluruh mahasiswa baru di Universitas Pendidikan Indonesia, jadi siap-siap ya!!",
+  },
+  {
+    id: "item-5",
+    question: "Apa yang Perlu Disiapkan untuk MABIMRPL?",
+    answer: "Untuk ikut Mabim itu sebenernya cuma perlu satu hal, yaitu Niat. Sisanya tinggal ikutin instruksi dari Mentornya aja deh :D",
+  },
+  {
+    id: "item-6",
+    question: "MABIMRPL itu Santai apa Serius Banget?",
+    answer: "Mabim ini ada waktunya santai, tapi juga ada waktu seriusnya. Jadi ga usah khawatir, ga akan seserius itu kok :)",
+  },
+];
+
+export function FAQAccordions() {
+  const [activeIndex, setActiveIndex] = useState("");
+
+  return (
+    <AccordionPrimitive.Root defaultValue="item-1" onValueChange={setActiveIndex} type="single" collapsible className="min-w-[343px]  mx-4 max-w-screen-md font-montserrat text-white z-[6] ">
+      {faqs.map((faq) => (
+        <AccordionPrimitive.Item value={faq.id} key={faq.id} className="accordionItem  overflow-hidden rounded-md border border-white/20">
+          <AccordionPrimitive.Header className="flex">
+            <AccordionPrimitive.Trigger className="group flex flex-1 cursor-pointer items-center justify-between bg-[#010030]/10 px-4 py-3 text-left font-semibold transition-all hover:bg-[#010030]/20">
+              {faq.question}
+              <ChevronDownIcon className="h-4 w-4 transition-transform duration-300 ease-in-out group-data-[state=open]:rotate-180" />
+            </AccordionPrimitive.Trigger>
+          </AccordionPrimitive.Header>
+
+          <AccordionPrimitive.Content className="accordionContent data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down overflow-hidden">
+            <div className="bg-white/20 px-4 py-3">
+              <div className="text-sm">{faq.answer}</div>
+            </div>
+          </AccordionPrimitive.Content>
+        </AccordionPrimitive.Item>
+      ))}
+    </AccordionPrimitive.Root>
+  );
+}
--- src/components/common/faq-section.tsx
+++ src/components/common/faq-section.tsx
@@ -1,10 +1,13 @@
 import Bg from "@/assets/bgBiner.svg";
 import Image from "next/image";
+import { FAQAccordions } from "./faq-accordions";
 
 export function FaqSection() {
   return (
-    <section id="faq" className="bg-[radial-gradient(101.34%_99.57%_at_50.02%_0.43%,#010030_7%,#7226FF_55%,#3243A6_100%)] text-center flex min-h-[812px] h-screen w-full flex-col items-center pt-[120px] relative overflow-clip">
-      <h3 className="font-ethno text-white text-[32px] z-[6]">faq</h3>
+    <section id="faq" className="bg-[radial-gradient(101.34%_99.57%_at_50.02%_0.43%,#010030_7%,#7226FF_55%,#3243A6_100%)] text-center flex  min-h-screen w-full flex-col items-center pt-[120px] relative overflow-clip">
+      <h3 className="font-ethno text-white text-[32px] z-[6] mb-10">faq</h3>
+
+      <FAQAccordions />
 
       <div className="absolute inset-0 opacity-5 bg-repeat bg-center bg-[length:160px] z-[1]" style={{ backgroundImage: `url(${Bg.src})` }} />
     </section>
