8493be792d0700e2b2ae741a14206cbfa3202230
Bintang Kurniawan <bintang.kurniawannn.98@gmail.com>
2025-09-22 13:42:24 +0700

    feat: kelompok

--- src/components/common/faq-section.tsx
+++ src/components/common/faq-section.tsx
@@ -5,7 +5,7 @@ import { FAQAccordions } from "./faq-accordions";
 export function FaqSection() {
   return (
     <section id="faq" className="bg-[radial-gradient(101.34%_99.57%_at_50.02%_0.43%,#010030_7%,#7226FF_55%,#3243A6_100%)] text-center flex  min-h-screen w-full flex-col items-center pt-[120px] relative overflow-clip">
-      <h3 className="font-ethno text-white text-[32px] z-[6] mb-10">faq</h3>
+      <h3 className="font-ethno text-white text-[32px] z-[6] mb-10 text-primary">faq</h3>
 
       <FAQAccordions />
 
