f7dd54fae3e07891e30d4b75123e0877d90b2390
Bintang Kurniawan <bintang.kurniawannn.98@gmail.com>
2025-09-22 13:59:44 +0700

    minor

--- src/components/ui/Dialog.tsx
+++ src/components/ui/Dialog.tsx
@@ -4,7 +4,6 @@ import * as React from "react";
 
 import { cn } from "@/lib/utils";
 import * as DialogPrimitive from "@radix-ui/react-dialog";
-import { Cross2Icon } from "@radix-ui/react-icons";
 
 const Dialog = DialogPrimitive.Root;
 
